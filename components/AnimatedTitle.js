5a242fec376dd77192967b70964376afb0b8b5ca
Bravoo Sonja <bravoosonja@gmail.com>
2022-07-22 07:40:08 +1000

    complete animated title

--- components/AnimatedTitle.js
+++ components/AnimatedTitle.js
@@ -2,6 +2,17 @@ import { motion } from "framer-motion";
 import styles from "../styles/components/AnimatedTitle.module.css";
 
 export default function AnimatedTitle({ textToAnimate }) {
+  const wordAnimation = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        delayChildren: 0.03,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
   const characterAnimation = {
     hidden: {
       opacity: 0,
@@ -19,29 +30,25 @@ export default function AnimatedTitle({ textToAnimate }) {
 
   return (
     <h1 className={styles.title} aria-label={textToAnimate} role="heading">
-      {textToAnimate.split(" ").map((word, index) => {
+      {textToAnimate.split(" ").map((character, index) => {
         return (
           <motion.span
             className={styles.word}
             aria-hidden="true"
             key={index}
-            // animate={textToAnimate}
-            transition={{
-              delayChildren: index * 0.06,
-              staggerChildren: 0.2,
-            }}
+            initial="hidden"
+            animate="visible"
+            variants={wordAnimation}
           >
-            {word.split("").map((character, index) => {
+            {character.split("").map((letter, index) => {
               return (
                 <motion.span
                   className={styles.character}
                   aria-hidden="true"
                   key={index}
-                  initial="hidden"
-                  animate="visible"
                   variants={characterAnimation}
                 >
-                  {character}
+                  {letter}
                 </motion.span>
               );
             })}
