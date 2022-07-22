a2aeb551f34d1de72b0f362dd63c86977241d2b2
Bravoo Sonja <bravoosonja@gmail.com>
2022-07-22 13:06:10 +1000

    Add mobile menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,51 @@
+import { useEffect, useState } from "react";
 import styles from "../styles/components/Navbar.module.css";
 import ActiveLink from "./ActiveLink";
+import Link from "next/link";
 
 export default function Navbar() {
+  const [windowDimension, setWindowDimension] = useState(null);
+
+  useEffect(() => {
+    setWindowDimension(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimension(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const isMobile = windowDimension <= 640;
   return (
     <nav>
-      <div className={styles.navbar}>
-        <div className={styles.navbarItems}>
-          <ActiveLink href="/contact">Contact</ActiveLink>
-
-          <ActiveLink href="/projects">Projects</ActiveLink>
-
-          <ActiveLink href="/about">About</ActiveLink>
-
-          <ActiveLink href="/">Home</ActiveLink>
+      {isMobile ? (
+        <div className={styles.mobileNavbar}>
+          <div className={styles.mobileNavbarItems}>
+            <Link href="/menu">
+              <h1>
+                <a>Menu</a>
+              </h1>
+            </Link>
+          </div>
+          <hr />
         </div>
-      </div>
+      ) : (
+        <div className={styles.navbar}>
+          <div className={styles.navbarItems}>
+            <ActiveLink href="/contact">Contact</ActiveLink>
+
+            <ActiveLink href="/projects">Projects</ActiveLink>
+
+            <ActiveLink href="/about">About</ActiveLink>
+
+            <ActiveLink href="/">Home</ActiveLink>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
---
+++ pages/menu.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function MobileMenu() {
+  return (
+    <div className="container black">
+      <div className="main">
+        <div className="menu">
+          <p>CLOSE</p>
+          <hr />
+          <h1>About</h1>
+          <h1>Projects</h1>
+          <h1>Contact</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
